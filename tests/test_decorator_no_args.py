ca835d44089bc9e10d03c1334d47dc1a67170f37
Dave Connell <39798556+connelldave@users.noreply.github.com>
2020-11-08 12:25:14 +0000

    Support **kwargs for decorated func

--- tests/test_decorator_no_args.py
+++ tests/test_decorator_no_args.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from unittest.mock import MagicMock
 
 import pytest
@@ -38,3 +39,15 @@ def test_decorated_func_passed_arg(patch_boto3_client) -> None:
     cove_output = simple_func("blue")
     # Two simple_func calls == two mock AWS accounts
     assert cove_output == ["blue", "blue"]
+
+
+def test_decorated_func_passed_arg_and_kwarg(patch_boto3_client) -> None:
+    @cove
+    def simple_func(session, time, colour, shape) -> Tuple[str, str, str]:
+        return colour, shape, time
+
+    cove_output = simple_func("11:11", shape="circle", colour="blue")
+    # Call with an arg and two kwargs
+    expected = [("blue", "circle", "11:11"), ("blue", "circle", "11:11")]
+    # Two simple_func calls == two mock AWS accounts
+    assert cove_output == expected
