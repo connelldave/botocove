7d46271502b07b6287adc66086fb49cede42f681
Iain Samuel McLean Elder <iain@isme.es>
2021-12-21 09:30:40 +0100

    Parameterize repetitions in demo

--- demo.py
+++ demo.py
@@ -1,37 +1,30 @@
+import sys
 import boto3
-from botocove import cove, CoveOutput
+from botocove import cove
 
-@cove()
+
 def get_iam_users(session):
     iam = session.client("iam", region_name="eu-west-1")
     all_users = iam.get_paginator("list_users").paginate().build_full_result()
     return all_users
 
+
 def main():
-    all_results = get_iam_users() 
 
-    if type(all_results) == CoveOutput:
-        # A list of dictionaries for each account, with account details included.
-        # Each account's get_iam_users return is in a "Result" key.
-        print(all_results["Results"]) 
+    member_account_id, repetitions = sys.argv[1:3]
+
+    target_ids = [member_account_id] * int(repetitions)
+
+    all_results = cove(get_iam_users, target_ids=target_ids)()
+
+    for r in all_results["Results"]:
+        print(r)
+
+    for e in all_results["Exceptions"]:
+        print(e)
         
-        # A list of dictionaries for each account that raised an exception
-        print(all_results["Exceptions"])
-
-        # A list of dictionaries for each account that could not be assumed into
-        print(all_results["FailedAssumeRole"])
-
-    else:
-        invalid_sessions, results, exceptions = all_results
-
-        for i in invalid_sessions:
-            print(i)
-        
-        for r in results:
-            print(r)
-        
-        for e in exceptions:
-            print(e)
+    for f in all_results["FailedAssumeRole"]:
+        print(f)
 
 
 if __name__ == "__main__":
