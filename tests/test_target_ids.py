05477c4a53d8de8d83684fe8ff11c55bef54f827
Iain Samuel McLean Elder <iain@isme.es>
2022-12-09 16:49:37 +0100

    Define behavior when target_ids is unspecified

--- tests/test_target_ids.py
+++ tests/test_target_ids.py
@@ -5,6 +5,17 @@ from botocove import cove
 from tests.moto_mock_org.moto_models import SmallOrg
 
 
+def test_when_unspecified_then_output_has_a_result_for_each_org_account(
+    mock_small_org: SmallOrg,
+) -> None:
+    @cove()
+    def do_nothing(session: Session) -> None:
+        pass
+
+    output = do_nothing()
+    assert set(mock_small_org.all_accounts) == {r["Id"] for r in output["Results"]}
+
+
 def test_when_account_not_in_org_raises_value_error(
     mock_small_org: SmallOrg,
 ) -> None:
