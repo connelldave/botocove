85f8c6fb7678a7e974a0a43cc558f27b3c9039eb
Iain Samuel McLean Elder <iain@isme.es>
2022-12-09 16:55:51 +0100

    Raise TypeError when target_ids is not list of str

--- tests/test_target_ids.py
+++ tests/test_target_ids.py
@@ -32,6 +32,17 @@ def test_when_accounts_in_org_then_output_has_result_for_each_target_id(
     assert set(some_org_accounts) == {r["Id"] for r in output["Results"]}
 
 
+def test_when_target_id_is_str_then_raises_type_error(mock_small_org: SmallOrg) -> None:
+    @cove(target_ids="111111111111")  # type: ignore[arg-type]
+    def do_nothing(session: Session) -> None:
+        pass
+
+    with pytest.raises(
+        TypeError, match=r"target_ids must be a list of str. Got str '111111111111'\."
+    ):
+        do_nothing()
+
+
 def test_when_account_not_in_org_raises_value_error(
     mock_small_org: SmallOrg,
 ) -> None:
